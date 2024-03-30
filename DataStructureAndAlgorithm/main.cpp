3eef4843ecac4c5fe69c0605b32e74760d855691
max <kalmro4@gmail.com>
2024-03-30 12:09:08 +0700

    Initial Commit

---
+++ DataStructureAndAlgorithm/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  DataStructureAndAlgorithm
+//
+//  Created by suppchai kalmro on 30/3/2567 BE.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
